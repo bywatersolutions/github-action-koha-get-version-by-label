84401b6059fd163c9c399b1d81069b481caf9e70
Kyle M Hall <kyle@bywatersolutions.com>
2021-11-03 09:09:48 -0400

    Use Koha community GitHub mirror instead of git.koha-community.org

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,12 +7,12 @@ echo "VERSION LABEL: $VERSION_LABEL"
 if [[ "$VERSION_LABEL" == "master" ]]; then
     KOHA_VERSION_BRANCH_NAME='master'
 elif [[ "$VERSION_LABEL" == "stable" ]]; then
-    KOHA_VERSION_LINE=$(git ls-remote --heads https://git.koha-community.org/Koha-community/Koha.git | grep '[0-9]\{2\}\.[0-9]\{2\}\.x' | tac | sed '1q;d')
+    KOHA_VERSION_LINE=$(git ls-remote --heads https://github.com/Koha-Community/Koha.git | grep '[0-9]\{2\}\.[0-9]\{2\}\.x' | tac | sed '1q;d')
     echo "KOHA VERSION LINE: $KOHA_VERSION_LINE"
     IFS='/' read -ra KOHA_VERSION_LINE_PARTS <<< "$KOHA_VERSION_LINE"
     KOHA_VERSION_BRANCH_NAME=${KOHA_VERSION_LINE_PARTS[2]}
 elif [[ "$VERSION_LABEL" == "oldstable" ]]; then
-    KOHA_VERSION_LINE=$(git ls-remote --heads https://git.koha-community.org/Koha-community/Koha.git | grep '[0-9]\{2\}\.[0-9]\{2\}\.x' | tac | sed '2q;d')
+    KOHA_VERSION_LINE=$(git ls-remote --heads https://github.com/Koha-Community/Koha.git | grep '[0-9]\{2\}\.[0-9]\{2\}\.x' | tac | sed '2q;d')
     echo "KOHA VERSION LINE: $KOHA_VERSION_LINE"
     IFS='/' read -ra KOHA_VERSION_LINE_PARTS <<< "$KOHA_VERSION_LINE"
     KOHA_VERSION_BRANCH_NAME=${KOHA_VERSION_LINE_PARTS[2]}
